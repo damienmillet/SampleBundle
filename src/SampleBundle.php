f47ae3e9ada4905182d93849154376e655f1883a
damienmillet <contact@damien-millet.xyz>
2020-08-02 16:04:43 +0200

    Update SampleBundle.php

--- src/SampleBundle.php
+++ src/SampleBundle.php
@@ -4,12 +4,17 @@ namespace DM\SampleBundle; // TODO : change for your NamespaceBundle, must to be
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class SampleBundle
+ * @package DM\SampleBundle
+ * @author damien millet
+ * @TODO : generate new file with 'composer init' or change this
+ * @TODO : change autoload & autoload-dev namespace
+ * @TODO : change repositories url
+ *
+ * contructor of bundle no need to be used
+ * 
+ */
 class SampleBundle extends bundle // TODO : change for your ClassNameBundle, must to be suffixed by Bundle
 {
-    /**
-     *  composer.json
-     *  TODO : generate new file with 'composer init' or change this
-     *  TODO : change autoload & autoload-dev namespace
-     *  TODO : change repositories url
-     */
 }
