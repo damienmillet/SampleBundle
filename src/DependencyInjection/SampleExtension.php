3f2b5b084a66dc886abb87b2a9e6d2966d43d39d
damienmillet <contact@damien-millet.xyz>
2020-08-02 16:09:55 +0200

    Update SampleExtension.php

--- src/DependencyInjection/SampleExtension.php
+++ src/DependencyInjection/SampleExtension.php
@@ -11,10 +11,17 @@ class SampleExtension extends Extension // TODO : change for your class, must be
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader(
-            $container,
-            new FileLocator(__DIR__.'/../Resources/config')
-        );
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+        
+        // define the must be tree
+        $configuration = $this->getConfiguration($configs, $container) ?? new Configuration();
+        // construct and compare the tree with receive configs
+        $config = $this->processConfiguration($configuration, $configs);
+        // inject the dynamic's arguments by classes
+        $definition = $container->getDefinition('dm_sample.sample');
+        // can use (int,$config[] ) where int = position number of entry but so restrictif
+        // config_sample must be CONFIG_SAMPLE in a /config/packages/sample.yaml
+        $definition->setArgument('$sample', $config['config_sample']);
     }
 }
